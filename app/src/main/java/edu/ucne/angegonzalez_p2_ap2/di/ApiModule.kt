6d6e4aa3360eee3b6d24a7a2eb01884eb2dfa80a
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-02-26 19:03:19 -0400

    Creando ApiModule

---
+++ app/src/main/java/edu/ucne/angegonzalez_p2_ap2/di/ApiModule.kt
@@ -0,0 +1,35 @@
+package edu.ucne.angegonzalez_p2_ap2.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.angegonzalez_p2_ap2.data.remote.DepositoManagerApi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ApiModule {
+   private const val BASE_URL = "https://sagapi-dev.azurewebsites.net/"
+
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesDepositoManagerApi(moshi: Moshi): DepositoManagerApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(DepositoManagerApi::class.java)
+    }
+}
