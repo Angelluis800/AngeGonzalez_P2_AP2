134aa1afa370ca1f05171368f3d08b503653f5c7
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-02-26 19:56:36 -0400

    Agregando la lista de depositos, isLoading y errorMessage al uiState

--- app/src/main/java/edu/ucne/angegonzalez_p2_ap2/presentation/deposito/DepositoUiState.kt
+++ app/src/main/java/edu/ucne/angegonzalez_p2_ap2/presentation/deposito/DepositoUiState.kt
@@ -1,5 +1,6 @@
 package edu.ucne.angegonzalez_p2_ap2.presentation.deposito
 
+import edu.ucne.angegonzalez_p2_ap2.data.remote.dto.DepositoDto
 import java.util.Date
 
 data class DepositoUiState(
@@ -8,4 +9,8 @@ data class DepositoUiState(
     val idCuenta: Int = 0,
     val concepto: String = "",
     val monto: Double = 0.0,
+    val listaDepositos: List<DepositoDto> = emptyList(),
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null,
+
 )
