5939d393eb54a45edfd4f620e00fe30acb35f505
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-02-26 19:36:29 -0400

    Cambiando Tipo de Dato a la propiedad Fecha a Date

--- app/src/main/java/edu/ucne/angegonzalez_p2_ap2/data/remote/dto/DepositoDto.kt
+++ app/src/main/java/edu/ucne/angegonzalez_p2_ap2/data/remote/dto/DepositoDto.kt
@@ -1,8 +1,10 @@
 package edu.ucne.angegonzalez_p2_ap2.data.remote.dto
 
+import java.util.Date
+
 data class DepositoDto(
     val idDeposito: Int,
-    val fecha: String,
+    val fecha: Date? = null,
     val idCuenta: Int,
     val concepto: String,
     val monto: Double,
