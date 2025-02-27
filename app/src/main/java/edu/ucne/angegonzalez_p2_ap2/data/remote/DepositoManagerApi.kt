2810ab30e7a813727c2e495378ae0d0355e1a8a7
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-02-26 20:31:47 -0400

    Agregando el ApiKey = test

--- app/src/main/java/edu/ucne/angegonzalez_p2_ap2/data/remote/DepositoManagerApi.kt
+++ app/src/main/java/edu/ucne/angegonzalez_p2_ap2/data/remote/DepositoManagerApi.kt
@@ -8,24 +8,30 @@ import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.Response
+import retrofit2.http.Headers
 
 interface DepositoManagerApi {
 
+    @Headers("X-API-Key:test")
     @GET("api/Depositos")
     suspend fun getDepositos(): Response<List<DepositoDto>>
 
+    @Headers("X-API-Key:test")
     @GET("api/Depositos/{id}")
     suspend fun getDeposito(@Path("id") id: Int): Response<DepositoDto>
 
+    @Headers("X-API-Key:test")
     @POST("api/Depositos")
     suspend fun saveDeposito(@Body depositoDto: DepositoDto): Response<DepositoDto>
 
+    @Headers("X-API-Key:test")
     @PUT("api/Depositos/{id}")
     suspend fun updateDeposito(
         @Path("id") id: Int,
         @Body depositoDto: DepositoDto
     ): Response<DepositoDto>
 
+    @Headers("X-API-Key:test")
     @DELETE("api/Depositos/{id}")
     suspend fun deleteDeposito(@Path("id") id: Int): Response<Unit>
 }
