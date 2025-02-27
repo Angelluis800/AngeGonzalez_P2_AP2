ebddecb8d2667f6a0b04df7ebfaf0dde8b3e4726
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-02-26 20:13:56 -0400

    Creando NavHost

---
+++ app/src/main/java/edu/ucne/angegonzalez_p2_ap2/presentation/navigation/ParcialNavHost.kt
@@ -0,0 +1,33 @@
+package edu.ucne.angegonzalez_p2_ap2.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import edu.ucne.angegonzalez_p2_ap2.presentation.deposito.DepositoListScreen
+
+@Composable
+fun ParcialNavHost(
+    navHostController: NavHostController
+) {
+    NavHost(
+        navController = navHostController,
+        startDestination = Screen.DepositoListScreen
+    ) {
+        composable<Screen.DepositoListScreen> {
+            DepositoListScreen(
+                createDeposito = { navHostController.navigate(Screen.DepositoScreen(0)) },
+                goToMenu = {navHostController.navigateUp()},
+                goToDeposito = { navHostController.navigate(Screen.DepositoScreen(it)) }
+            )
+        }
+        composable<Screen.DepositoScreen> {
+            val depositoId = it.toRoute<Screen.DepositoScreen>().depositoId
+//            DepositoScreen(
+//                depositoId = depositoId,
+//                goBackToList = { navHostController.navigateUp() }
+//            )
+        }
+    }
+}
